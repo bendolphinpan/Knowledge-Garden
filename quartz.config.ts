f12198ccb49a5c70996e96742a8cabf1d14ede92
bendolphinpan <phy5543@163.com>
2025-04-26 15:24:19 +0800

    Update quartz.config.ts

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "Quartz 4",
+    pageTitle: "🏠老魔王的基地",
     pageTitleSuffix: "",
     enableSPA: true,
     enablePopovers: true,
